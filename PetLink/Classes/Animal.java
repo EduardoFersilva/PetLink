e5bb3b1b928040f2ea274525f0a0fd89a2a4feb0
Michael Evangelista <115849478+MaikeRhoads@users.noreply.github.com>
2023-09-20 23:58:11 +0000

    comeco da criacao das classes

--- PetLink/Classes/Animal.java
+++ PetLink/Classes/Animal.java
@@ -1,7 +1,16 @@
 public class Animal {
+    int idAnimal;
+    int idOng;
+    int idAdotante;
 
-
-
+    String tipoAnimal;
+    String racaAnimal;
+    String porteAnimal;
+    char sexoAnimal;
+    Date idadeAnimal;
+    String nomeAnimal;
+    boolean statusAnimal;
+    String obsAnimal;
 
 
 
