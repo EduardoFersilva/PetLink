7c2c0cda7db24c3675cfb57d0aef32fc7cba026b
Rafinharibeiro <rafa_1310@icloud.com>
2023-10-11 10:52:18 -0300

    update ok

--- PetLink/src/main/java/br/com/petlink/dao/PetDao.java
+++ PetLink/src/main/java/br/com/petlink/dao/PetDao.java
@@ -98,6 +98,30 @@ public class PetDao {
         System.out.println("fail in database connection");
     }
 }
+    public void updatePet(Pet pet) {
+
+        String SQL = " UPDATE PET  SET NAME = ? WHERE ID = ?" ;
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa","sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            preparedStatement.setString(1, pet.getName());
+            preparedStatement.setString(2, pet.getId());
+            preparedStatement.execute();
+            System.out.println("success in insert pet");
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println("fail in database connection");
+            System.out.println("Error: " + e.getMessage());
+
+        }
+    }
+
 }
 
 
--- PetLink/src/main/java/br/com/petlink/servlet/CreatePetServlet.java
+++ PetLink/src/main/java/br/com/petlink/servlet/CreatePetServlet.java
@@ -17,11 +17,18 @@ public class CreatePetServlet  extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String petId = request.getParameter("id");
         String petName = request.getParameter("pet-name");
 
-       Pet pet = new Pet(petName);
-       new PetDao().createPet(pet);
+       Pet pet = new Pet(petId,petName);
+       PetDao petDao = new PetDao();
 
+        if (petId.isBlank()) {
+            petDao.createPet(pet);
+        } else {
+            petDao.updatePet(pet);
+
+        }
 
         response.sendRedirect("/find-all-pets");
     }
