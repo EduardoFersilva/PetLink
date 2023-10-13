49bc499b618e207932c2a259cc5c6da8f87a6d95
Rafinharibeiro <rafa_1310@icloud.com>
2023-10-13 17:44:30 -0300

    add exceptions

--- PetLink/src/main/java/br/com/petlink/dao/UserDao.java
+++ PetLink/src/main/java/br/com/petlink/dao/UserDao.java
@@ -1,14 +1,51 @@
 package br.com.petlink.dao;
 
+import br.com.petlink.config.ConnectionPoolConfig;
 import br.com.petlink.model.Users;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDao {
 
     public boolean verifyCredentials(Users users) {
 
-        String SQL = "";
+        String SQL = "SELECT * FROM USERS WHERE USERNAME = 'rafael'";
 
+        try{
+
+            Connection connection = ConnectionPoolConfig.getConnection();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            preparedStatement.setString(1, users.getUserName());
+           ResultSet resultSet =  preparedStatement.executeQuery();
+            System.out.println("sucess in select username");
+
+           while(resultSet.next()){
+
+              String password =  resultSet.getString("password");
+
+              if(password.equals(users.getPassword())){
+
+                  return true;
+
+              }
+
+           }
+           connection.close();
+            return false;
+
+
+
+        } catch (Exception e){
+                System.out.println("Error " +  e.getMessage());
                 return false;
+
+
     }
 
+}
 }
