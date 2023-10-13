238c015f2b6e293f06bbced4388b3aedd6b36071
Rafinharibeiro <rafa_1310@icloud.com>
2023-10-13 17:21:06 -0300

    add userDao

---
+++ PetLink/src/main/java/br/com/petlink/dao/UserDao.java
@@ -0,0 +1,14 @@
+package br.com.petlink.dao;
+
+import br.com.petlink.model.Users;
+
+public class UserDao {
+
+    public boolean verifyCredentials(Users users) {
+
+        String SQL = "";
+
+                return false;
+    }
+
+}
